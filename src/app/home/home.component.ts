649de9724537247dbcca8705ad137045be1012f0
PeterElishaa <106022682+PeterElishaa@users.noreply.github.com>
2024-10-18 11:38:43 -0900

    Add files via upload

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',    // Ensure this points to your HTML file
+  styleUrls: ['./home.component.css'],      // Ensure this points to your component-specific CSS file
+})
+export class HomeComponent {}
